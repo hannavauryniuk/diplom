d0dbfa343a9f022a1ceeb9961c4d9743328a08fa
Evgeniya <evgeniova@gmail.com>
2017-01-23 01:00:08 +0300

    routing added

--- app/app.js
+++ app/app.js
@@ -1,4 +1,19 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', ['uiGmapgoogle-maps']);
+var app = angular.module('myApp', [
+    'uiGmapgoogle-maps',
+    'ngRoute'
+]);
+app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+    $routeProvider
+        .when ('/', {
+            templateUrl : 'views/home.html'
+            }        )
+        .when('/main', {
+            templateUrl : 'views/main.html',
+            controller: 'checkWeatherController'
+        })
+        .otherwise({redirectTo: '/errorPage.html'})
+}]);
